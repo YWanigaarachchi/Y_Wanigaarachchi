f2247e89b99d80df3eed5f50571bde07f588a199
Yasas Wanigaarachchi <168370336+YWanigaarachchi@users.noreply.github.com>
2025-08-07 02:07:59 +0530

    added to js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Scroll Progress Script
+window.addEventListener('scroll', () => {
+    const scrollProgress = document.getElementById('scrollProgress');
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (scrollTop / scrollHeight) * 100;
+    scrollProgress.style.width = scrolled + '%';
+});
